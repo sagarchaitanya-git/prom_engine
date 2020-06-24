cd09a4e092bfcbf38273af55625457ee4b574b0a
sagarchaitanya-git <43842680+sagarchaitanya-git@users.noreply.github.com>
2020-06-24 09:56:54 +0530

    Update program.cs
    Reading all SKU quantity inputs

--- program.cs
+++ program.cs
@@ -8,8 +8,18 @@ public class Program
 	private static int SKUID_PRICE_D=15;
 	public static void Main()
 	{
+		int A_Quant;
+		int B_Quant;
+		int C_Quant;
+		int D_Quant;
 		Console.WriteLine("Please enter how many products you need for SKUID A");
-		int A_Quant = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine(SKUID_PRICE_A*A_Quant); 
+		A_Quant = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter how many products you need for SKUID B");
+		B_Quant = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter how many products you need for SKUID C");
+		C_Quant = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter how many products you need for SKUID D");
+		D_Quant = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine(SKUID_PRICE_A*A_Quant);
 	}
 }
